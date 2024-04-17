820f6c7b172e044fb8ae7e164fd34f493bdf31e5
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-04-17 14:54:06 +0000

    podstawa z poprzedniego zadania

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+        System.out.print("wprowadz liczbe wierszy ");
+        int x = scanner.nextInt();
+
+        System.out.print("wprowadz liczbe kolumn");
+        int y = scanner.nextInt();
+
+        char znak = '*';
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(znak + " ");
+            }
+            System.out.println();
+        }
+    }
 }
